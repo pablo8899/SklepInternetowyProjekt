d34a48e6e626395ce8b9c3001a33ac0d515e4dbe
Paweł Wójsik <pablo8899@interia.pl>
2020-12-09 15:00:39 +0100

    initial commit

---
+++ SklepInternetowy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SklepInternetowy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
